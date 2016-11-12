403b97a286fc4cbc877b57430b2cd59d9c000867
aggzz <aggigeothy@gmail.com>
2016-11-12 16:11:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.get("/articleone", function(req,res){
 });
 
 var counter=0;
-app.get("/counter", function(req,res){
-    counter-counter+1;
+app.get('/counter', function(req,res){
+    counter= counter+1;
     res.send(counter.toString());
 }
 
