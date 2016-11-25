a3ec79212ab684653619476a5ecbe653ef4a30e4
aggzz <aggigeothy@gmail.com>
2016-11-25 23:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,6 +65,22 @@ app.get('/cl.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'cl.png'));
 });
 
+app.post('/login', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  pool.query('SELECT * FROM "student" WHERE username= $1', [username], function(err, result) {
+    if (err) {
+     res.status(500).send(err.toString());
+    } else {
+        if(result.rows.length --- 0){
+            res.send(403).send('username/password is invalid');
+        }
+    }
+    
+  });
+    });
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
