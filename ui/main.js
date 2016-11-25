858864dce9adf6055e1a2ece02f4a1bf15dc6dfa
aggzz <aggigeothy@gmail.com>
2016-11-25 22:23:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,33 @@
-console.log('Loaded!');
-//counter
-var counter=0;
-var button= document.getElementById("counter");
-button.onclick = function() {
-    //Make requst tothe counter end point
+
+//submit userid//and password here
+varsubmit= document.getElementById('submit_btn');
+submit.onclick = function() {
+    //create request object
+    var request =new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLhttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                alert("logged in successfully");
+                
+            }else if( request.status === 403){
+                alert("username/password is incorrect");
+            }else if( request.status === 500){
+                alert("something went wrong in the server");
+                
+            }
+        }
+    }
     
-    //Render the variablein the correct span.
-    counter = counter+1;
-    var span= document.getElementById("counter");
-    span.innerHTML=counter.toString();
-}
+    //make the request
+    
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://aggzz.imad.hasura-app.io/fac.html', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password: password}));
+};
